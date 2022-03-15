c4a4d78e64ca8e8b83dc333d57d3d224b51f112b
Dari <101057112+Guldariya-Kurbonova@users.noreply.github.com>
2022-03-15 22:40:31 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame
+
+#Initialize the pygame
+pygame.init()
+
+#create the screen
+screen = pygame.display.set_mode((800, 600,))
+
+#game loop
+running = True 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running == False
+            
